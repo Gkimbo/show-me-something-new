13b1b6fb8ae51312694e381f02db681647b9593c
Gavin Kimball <you@example.com>
2023-08-23 11:32:59 -0400

    add style

---
+++ client/src/services/popularCitySearchData.js
@@ -0,0 +1,14 @@
+const cities = [
+    { value: "New York", label: "New York" },
+    { value: "Paris, France", label: "Paris, France" },
+    { value: "Tokyo, Japan", label: "Tokyo, Japan" },
+    { value: "London, UK", label: "London, UK" },
+    { value: "Rome, Italy", label: "Rome, Italy" },
+    { value: "Dubai, UAE", label: "Dubai, UAE" },
+    { value: "Sydney, Australia", label: "Sydney, Australia" },
+    { value: "Rio de Janeiro, Brazil", label: "Rio de Janeiro, Brazil" },
+    { value: "Shanghai, China", label: "Shanghai, China" },
+    { value: "Cape Town, South Africa", label: "Cape Town, South Africa" },
+];
+
+export default cities;
